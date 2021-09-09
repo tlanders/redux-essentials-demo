1c36f7a0953274da8a7df47717497f510f3df32e
Troy Landers <tlanders@landersconsulting.biz>
2021-09-08 19:13:36 -0500

    normalized users using createEntityAdaptor

--- src/essentials-feed-app/features/users/usersSlice.js
+++ src/essentials-feed-app/features/users/usersSlice.js
@@ -1,7 +1,9 @@
-import {createAsyncThunk, createSlice} from "@reduxjs/toolkit";
+import {createAsyncThunk, createEntityAdapter, createSlice} from "@reduxjs/toolkit";
 import {client} from "../../api/client";
 
-const initialState = [];
+const usersAdapter = createEntityAdapter();
+
+const initialState = usersAdapter.getInitialState();
 
 export const fetchUsers = createAsyncThunk('users/fetchUsers', async () => {
     const response = await client.get('/fakeApi/users');
@@ -12,14 +14,14 @@ const usersSlice = createSlice({
     name: 'users',
     initialState,
     reducers: {},
-    extraReducers: {
-        [fetchUsers.fulfilled]: (state, action) => {
-            return action.payload;
-        }
+    extraReducers(builder) {
+        builder.addCase(fetchUsers.fulfilled, usersAdapter.setAll)
     }
 });
 
-export const selectAllUsers = state => state.users;
-export const selectUserById = (state, userId) => state.users.find(user => user.id === userId);
+export const {
+    selectAll: selectAllUsers,
+    selectById: selectUserById
+} = usersAdapter.getSelectors(state => state.users);
 
 export default usersSlice.reducer;
